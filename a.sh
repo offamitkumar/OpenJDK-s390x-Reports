fbe0e071fa0d0e6aea5155ee5e4f8ef7a2cc9897
Amit Kumar <amit.kumar220@ibm.com>
2025-04-22 04:46:55 +0000

    release fix

--- a.sh
+++ a.sh
@@ -294,7 +294,7 @@ jdk_release() {
   export CONF=linux-s390x-server-release
 
   bash configure \
-    --with-boot-jdk=$HOME/boot_jdk_23 \
+    --with-boot-jdk=$HOME/boot_jdk_24 \
     --with-jtreg=$HOME/jtreg \
     --with-gtest=$HOME/googletest \
     --with-debug-level=release \
@@ -305,7 +305,7 @@ jdk_release() {
   make dist-clean;
 
   bash configure \
-    --with-boot-jdk=$HOME/boot_jdk_23 \
+    --with-boot-jdk=$HOME/boot_jdk_24 \
     --with-jtreg=$HOME/jtreg \
     --with-gtest=$HOME/googletest \
     --with-debug-level=release \
